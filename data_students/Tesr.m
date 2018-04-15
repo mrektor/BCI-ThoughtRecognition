f773207827e9379d3071b5788d72b2d4bb16ee19
Ettore <mr.ektor@gmail.com>
2018-04-15 20:38:08 +0200

    Schedule update

--- data_students/Tesr.m
+++ data_students/Tesr.m
@@ -1,12 +1,25 @@
 %%
+figure()
+for k=1:10
 
-vediamo = nirs.oxy
+plot(mean(yes_signal{k},2),'--')
+end
+%%
+test = yes_signal{1}
 
 %%
+yes_80 = yes_signal
 
-plot(nirs.timePoints,vediamo(:,1)); hold on;
-plot(nirs.timePoints,nirs.dxy(:,1))
+%%
+tot_yes = yes_80{1};
+for k=2:length(yes_80)
+    tot_yes = [tot_yes; yes_80{k}];
+end
 
 
 %%
-mean(nirs.timePoints,vediamo(:,1))
+[coeff,score,latent,tsquared,explained,mu] = pca(tot_yes)
+
+bar(explained); grid on
+figure();
+bar(cumsum(explained)); grid minor
